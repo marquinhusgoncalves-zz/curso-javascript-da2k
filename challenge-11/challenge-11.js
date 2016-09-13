3ed8c57567aeee48d6586b31de57ee1b7575d60a
Marcus Goncalves <marcus.goncalves1@walmart.com>
2016-09-13 18:41:46 -0300

    OK

--- challenge-11/challenge-11.js
+++ challenge-11/challenge-11.js
@@ -79,7 +79,7 @@
   console.log( 'Pares de 0 a 20:' );
   var numbers = [];
   for(var i = 0; i <=20; i++) {
-    if(i % 2 == 0)
+    if(i % 2 !== 0)
     numbers.push(i);
   }
   console.log(numbers);
