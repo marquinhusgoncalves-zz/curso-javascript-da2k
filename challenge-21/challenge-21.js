13b0c99fb7a6b48e078f7daae16795c29de5e4f9
Marcus Goncalves <marcus.goncalves1@walmart.com>
2016-11-28 16:54:23 -0200

    Challenge-21

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -15,3 +15,44 @@ usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
 dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 */
 // ?
+(function( window, document ) {
+  'use strict';
+
+  var $buttonStart = document.querySelector( ' button[data-js="button-start"] ' );
+  var $buttonStop = document.querySelector( 'button[data-js="button-stop"]' );
+  var $buttonReset = document.querySelector( 'button[data-js="button-reset"]' );
+  var $input = document.querySelector( ' input[type="text"] ' );
+  var cron;
+  var started = false;
+
+  $buttonStart.addEventListener( 'click', function( event ) {
+    event.preventDefault();
+    if ( !started )
+      cron = window.setInterval( startWatch, 1000 );
+    started = true;
+  } );
+
+  function startWatch() {
+    $input.value++;
+  }
+
+  $buttonStop.addEventListener( 'click', function( event ) {
+    event.preventDefault();
+    clearInterval( cron );
+    started = false;
+  } );
+
+  $buttonReset.addEventListener( 'click', function( event ) {
+    event.preventDefault();
+    clearInterval( cron );
+    $input.value = 0;
+    started = false;
+  } );
+
+})( window, document );
+
+// function timer() {
+//     $input.value++;
+//     window.setTimeout( timer, 1000)
+//   }
+// timer();
