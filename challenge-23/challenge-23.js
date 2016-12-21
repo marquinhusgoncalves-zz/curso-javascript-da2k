e943159a2fbc678bc9eba9866240b8688dfc595b
Marcus Goncalves <marcus.goncalves1@walmart.com>
2016-12-21 21:14:15 -0200

    Challenge-23

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -30,6 +30,14 @@ var $visor = document.querySelector( '[data-js="visor"]' );
 var $buttonsNumbers = document.querySelectorAll( '[data-js="button-number"]' );
 var $buttonsOperations = document.querySelectorAll( '[data-js="button-operation"]' );
 var $buttonCE = document.querySelector( '[data-js="button-ce"]' );
+var $buttonEqual = document.querySelector( '[data-js="button-equal"]' )
+
+
+$buttonCE.addEventListener( 'click', handleClickCe, false);
+
+function handleClickCe() {
+  $visor.value = 0;
+}
 
 Array.prototype.forEach.call( $buttonsNumbers, function( button ) {
   button.addEventListener( 'click', handleClickNumber, false );
@@ -48,86 +56,27 @@ Array.prototype.forEach.call( $buttonsOperations, function( button ) {
 })
 
 function handleClickOperation() {
-  var operations = ['+', '-', '*', '/'];
-  if( isLastItemAnOperation( operations ) ){
-    $visor.value = $visor.value.slice(0, -1);
-  }
+  removeLastItemIfItIsAnOperator();
   $visor.value += this.value;
 }
-function isLastItemAnOperation( operations ) {
-
+function removeLastItemIfItIsAnOperator() {
+  if( isLastItemAnOperation() ) {
+    $visor.value = $visor.value.slice(0, -1);
+  }
 }
 
-$buttonCE.addEventListener( 'click', handleClickCe, false);
-
-function handleClickCe() {
-  $visor.value = 0;
+function isLastItemAnOperation() {
+  var operations = ['+', '-', 'x', '÷'];
+  var lastItem = $visor.value.split('').pop();
+  return operations.some( function( operador ) {
+    return operador === lastItem;
+  });
 }
 
-var $ce = document.querySelector( 'button[name="ce"]' );
-var $zero = document.querySelector( 'button[name="zero"]' );
-var $one = document.querySelector( 'button[name="one"]' );
-var $two = document.querySelector( 'button[name="two"]' );
-var $three = document.querySelector( 'button[name="three"]' );
-var $four = document.querySelector( 'button[name="four"]' );
-var $five = document.querySelector( 'button[name="five"]' );
-var $six = document.querySelector( 'button[name="six"]' );
-var $seven = document.querySelector( 'button[name="seven"]' );
-var $eight = document.querySelector( 'button[name="eight"]' );
-var $nine = document.querySelector( 'button[name="nine"]' );
-var $sum = document.querySelector( 'button[name="sum"]' );
-var $sub = document.querySelector( 'button[name="sub"]' );
-var $div = document.querySelector( 'button[name="div"]' );
-var $mult = document.querySelector( 'button[name="mult"]' );
-var $equal = document.querySelector( 'button[name="equal"]' );
+$buttonEqual.addEventListener('click', handleClickEqual, false);
 
-// $ce.addEventListener( 'click', function( event ) {
-//   document.querySelector( 'input[name="visor"]' ).value = 0;
-// } )
-// $zero.addEventListener( 'click', function( event ) {
-//   document.querySelector( 'input[name="visor"]' ).value += 0;
-// } )
-// $one.addEventListener( 'click', function( event ) {
-//   document.querySelector( 'input[name="visor"]' ).value += 1;
-// } )
-// $two.addEventListener( 'click', function( event ) {
-//   document.querySelector( 'input[name="visor"]' ).value += 2;
-// } )
-// $three.addEventListener( 'click', function( event ) {
-//   document.querySelector( 'input[name="visor"]' ).value += 3;
-// } )
-// $four.addEventListener( 'click', function( event ) {
-//   document.querySelector( 'input[name="visor"]' ).value += 4;
-// } )
-// $five.addEventListener( 'click', function( event ) {
-//   document.querySelector( 'input[name="visor"]' ).value += 5;
-// } )
-// $six.addEventListener( 'click', function( event ) {
-//   document.querySelector( 'input[name="visor"]' ).value += 6;
-// } )
-// $seven.addEventListener( 'click', function( event ) {
-//   document.querySelector( 'input[name="visor"]' ).value += 7;
-// } )
-// $eight.addEventListener( 'click', function( event ) {
-//   document.querySelector( 'input[name="visor"]' ).value += 8;
-// } )
-// $nine.addEventListener( 'click', function( event ) {
-//   document.querySelector( 'input[name="visor"]' ).value += 9;
-// } )
-// $sum.addEventListener( 'click', function( event ) {
-//   document.querySelector( 'input[name="visor"]' ).value += '+';
-// } )
-// $sub.addEventListener( 'click', function( event ) {
-//   document.querySelector( 'input[name="visor"]' ).value += '-';
-// } )
-// $div.addEventListener( 'click', function( event ) {
-//   document.querySelector( 'input[name="visor"]' ).value += '/';
-// } )
-// $mult.addEventListener( 'click', function( event ) {
-//   document.querySelector( 'input[name="visor"]' ).value += '*';
-// } )
-// $equal.addEventListener( 'click', function( event ) {
-//   document.querySelector( 'input[name="visor"]' ).value = eval( document.querySelector( 'input[name="visor"]' ).value );
-// } )
+function handleClickEqual() {
+  removeLastItemIfItIsAnOperator();
+}
 
 })();
