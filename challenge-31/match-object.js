9b0234ad5c6fe4973a868679cfe8720454d6ed6f
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2017-05-19 20:09:26 -0300

    OK

---
+++ challenge-31/match-object.js
@@ -0,0 +1,19 @@
+console.log(Math.PI);
+
+console.log(Math.abs(-10));
+
+console.log(Math.ceil(11.3));
+
+console.log(Math.floor(11.3));
+
+console.log(Math.round(11.5));
+
+console.log(Math.sqrt(16));
+
+console.log(Math.cbrt(27));
+
+console.log(Math.max(-1, 0, 1, 2, 3, 4));
+
+console.log(Math.min(-1, 0, 1, 2, 3, 4));
+
+console.log(Math.random());
